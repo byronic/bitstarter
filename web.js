eace65e1b008b2d4391a8f8aea1584a5a6a63dbe
Byron Lagrone <byron.lagrone@gmail.com>
2013-07-05 17:59:01 -0500

    Add index.html and code to web.js to serve index and INDEX ALONE. No other pages will be served. No lines, no waiting.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+// load in file as buffer
+var bIndex = fs.readFileSync("index.html");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('HELLO WORD 2<br />THE REVENGENING');});
+    response.send(bIndex.toString());});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
