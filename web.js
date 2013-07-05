a09d3637a8a99503023266f61067c29daee0d152
Byron Lagrone <byron.lagrone@gmail.com>
2013-07-05 17:46:18 -0500

    Modified web.js in a manner similar to Hello World 2 as described. Also added a line break HTML tag to see how it dealt.

--- web.js
+++ web.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+  response.send('HELLO WORD 2<br />THE REVENGENING');});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
