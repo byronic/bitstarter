95a948a91e3fc57d597e7969c064d3d75e64e726
Byron Lagrone <byron.lagrone@gmail.com>
2013-07-05 21:21:00 -0500

    This is sooo kludgy, there must be a better way... but it worked on the debug commands. :iiam:

--- grader.js
+++ grader.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /*
 
-Cut and pasted without comment from assignments.
+copied and pasted from the newer, second edition. I also figured out on my own through trial and error why my test case wasn't working. Note that you have to have the <not_necessarily_the_variable_name_but_indicative_for_humans> as part of the commander deal. >< sheesh. Can't believe  I wasted all that time.
 
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -27,9 +27,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var restler = require('restler');
+var URL_DEFAULT = "";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -59,15 +60,45 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+// check this!
+var checkWebAddress = function(url, checksfile)
+{
+    var out = {};
+    var test =  restler.get(url).on('complete', function(data)
+			{
+				$ = cheerio.load(data);
+				var checks = loadChecks(checksfile).sort();
+				for(var ii in checks)
+				{
+                                    var present = $(checks[ii]).length > 0;
+                                    out[checks[ii]] = present;
+				}
+			    console.log(JSON.stringify(out, null, 4));
+			});
+};
+
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+
 if(require.main == module) {
     program
-        .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
-        .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
-	.option('-u, --url ', 'URL of index.html', assertFileExists, HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <website_url>', 'Path to web address', String, URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson;
+    if(program.url == URL_DEFAULT)
+    {
+	checkJson = checkHtmlFile(program.file, program.checks);
+	outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+    else
+	checkWebAddress(program.url, program.checks);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
